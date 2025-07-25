668072e9da9723d4022f4184604c9613eabae79b
MartinezErwin <612110504@utsalamanca.edu.mx>
2025-07-24 22:57:49 -0600

    feat: Add padding to Illustration component for improved layout

--- resources/js/Components/Welcome/Illustration.jsx
+++ resources/js/Components/Welcome/Illustration.jsx
@@ -1,11 +1,9 @@
 export default function Illustration() {
     return (
-        <div className="relative flex items-center justify-center flex-1">
+        <div className="relative flex items-center justify-center flex-1 py-10 lg:py-0">
             <div className="relative">
-                {/* Círculos decorativos */}
                 <div className="absolute bg-blue-200 rounded-full -top-4 -left-4 w-72 h-72 mix-blend-multiply filter blur-xl opacity-70 animate-pulse"></div>
                 <div className="absolute delay-1000 bg-purple-200 rounded-full -bottom-4 -right-4 w-72 h-72 mix-blend-multiply filter blur-xl opacity-70 animate-pulse"></div>
-                {/* Imagen principal */}
                 <div className="relative z-10 p-8 bg-white shadow-2xl rounded-2xl">
                     <img
                         src="/Assets/computer.svg"
